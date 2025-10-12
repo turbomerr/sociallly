4f39b12fb673a9f2d8957f94651b7fdcf1faf731
Omer Gokbakar <turbofullstack.dev10@gmail.com>
2025-10-12 15:33:23 +0200

    SideBar User UI created

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,9 +1,12 @@
-import { currentUser } from "@clerk/nextjs/server"
+import { auth, currentUser } from "@clerk/nextjs/server"
 import UnAuthenticatedSidebar from "./UnAuthenticatedSidebar";
 
-import { Button } from "./ui/button";
 import { Separator } from "@/components/ui/separator"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { MapPin } from 'lucide-react';
+import { Link as LinkIcon } from 'lucide-react';
+import Link from "next/link";
+import { getUserByClerkId } from "@/actions/user.action"
 
 async function SideBar() {
 
@@ -12,33 +15,45 @@ async function SideBar() {
         return <UnAuthenticatedSidebar />
     }
 
+    const sideBarUser = await getUserByClerkId(authUser.id);
+    if (!sideBarUser) return null;
+    //console.log(sideBarUser)
+
+
     return (
-        <div className="sticky top-20 bg-transparent border rounded-lg py-4 w-full">
-            <div className="flex flex-col items-center space-y-2">
-                {/* <img className="rounded-full w-36 h-36" src="https://a.storyblok.com/f/322327/2586x1449/52797ccfc3/taycan.jpg/m/865x486/smart/filters:format(webp)" alt="profile" />
-                 */}
-                <Avatar className="w-36 h-36">
-                    <AvatarImage  src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
+        <div className="sticky top-20 bg-transparent border rounded-lg py-4 ">
+            <div className="flex flex-col items-center space-y-2 w-full">
+                
+                <Link href={`/profile/${sideBarUser.username}`} className="flex flex-col items-center space-y-2">
+                    <Avatar className="w-36 h-36 ">
+                    <AvatarImage src={authUser.imageUrl} />
+                    <AvatarFallback>Image</AvatarFallback>
                 </Avatar>
-                <h1 className="text-lg font-semibold ">Omer Gokbakar</h1>
-                <h3 className="font-light">Username</h3>
-                <div className="flex space-x-2 font-extralight ">
-                    <span>Bio</span>
-                    <span>@Website</span>
-                </div>
+                <h1 className="text-lg font-semibold ">{`${authUser.firstName} ${authUser.lastName}`}</h1>
+                <h3 className="font-light">{sideBarUser.username ?? authUser.emailAddresses[0].emailAddress.split("@")[0]}</h3>
+                </Link>
                 <Separator />
-                <div className="flex items-center justify-between w-full px-4">
+                <div className="flex items-center justify-between space-x-4">
                     <div className="flex flex-col text-center">
-                        <p>1</p>
+                        <p>{sideBarUser._count.following}</p>
                         <p>Following</p>
                     </div>
                     <div className="flex flex-col text-center">
-                        <span>1</span>
-                        <span>Following</span>
+                        <span>{sideBarUser._count.followers}</span>
+                        <span>Followers</span>
                     </div>
                 </div>
                 <Separator className="mx-4"/>
+                <div className="flex flex-col items-center w-full space-y-2">
+                    <div className="flex items-center gap-1 font-light">
+                        <MapPin className="w-4 h-4 " />
+                        <p>{sideBarUser.location || "No location"}</p>
+                    </div>
+                    <Link href="websitelink" className="flex items-center gap-1 font-light" target="_blank">
+                        <LinkIcon className="w-4 h-4" />
+                        {sideBarUser.website || "No Website"}
+                    </Link>
+                </div>
 
             </div>
         </div>
