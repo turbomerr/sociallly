4944c22c0f21dbfd918df3566023bd7a2ec8aa3c
Omer Gokbakar <turbofullstack.dev10@gmail.com>
2025-10-13 20:30:15 +0200

    uploadthing api created

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,31 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { auth } from "@clerk/nextjs/server";
+
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+    postImage: f({
+        image: {
+            maxFileSize: "4MB",
+            maxFileCount: 1,
+        }
+    })
+        .middleware(async () => {
+            const { userId } = await auth();
+
+            if (!userId) throw new Error("Unauthorized")
+            // Whatever is returned here is accessible in onUploadComplete as `metadata`
+            return { userId }
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            try {
+                return { fileUrl: file.url };
+            } catch (error) {
+                console.error("Error in onUploadComplete:", error);
+                throw error;
+            }
+        }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
